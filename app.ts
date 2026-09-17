1ff706b9c9cfc782faa732d0899079da23de6798
Santiago Ortiz <santiagoortizgomez621@gmail.com>
2026-09-17 13:50:44 -0500

    Subir estructura del proyecto

---
+++ app.ts
@@ -0,0 +1,18 @@
+import { Application, oakCors } from "./Dependencies/dependencias.ts";
+
+const app = new Application();
+app.use(oakCors({
+    origin:"*"
+}));
+
+
+// const routes = [resultadoRouter];
+
+// routes.forEach(router =>{
+//     app.use(router.routes());
+//     app.use(router.allowedMethods());
+// })
+
+console.log("Servidor corriendo por el puerto 8001");
+
+app.listen({port: 8001});
