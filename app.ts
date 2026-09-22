2da5b266404e00c7f2e1bee12f840bbcc7356b2c
jeffryrodriguez12-collab <jeffryrodriguez117@mail.com>
2026-09-22 13:35:19 -0500

    mantenimientos

--- app.ts
+++ app.ts
@@ -1,18 +1,20 @@
 import { Application, oakCors } from "./Dependencies/dependencias.ts";
+import { serviciosRouter } from "./Router/serviciosRouter.ts";
 
 const app = new Application();
+
 app.use(oakCors({
-    origin:"*"
+    origin: "*"
 }));
 
+// Registrar routers
+const routes = [serviciosRouter];
 
-// const routes = [resultadoRouter];
-
-// routes.forEach(router =>{
-//     app.use(router.routes());
-//     app.use(router.allowedMethods());
-// })
+routes.forEach(router => {
+    app.use(router.routes());
+    app.use(router.allowedMethods());
+});
 
 console.log("Servidor corriendo por el puerto 8001");
 
-app.listen({port: 8001});
+app.listen({ port: 8001 });
