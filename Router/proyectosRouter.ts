c7c469ecbb04693e1e514d259945f9ba250d77ba
Jhoan Cotamo <jhoan.cotamo42@gmail.com>
2026-09-24 14:13:41 -0500

    Uso de middleware y proteccion de operaciones HTTP

--- Router/proyectosRouter.ts
+++ Router/proyectosRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from "../Dependencies/dependencias.ts";
 import { listarProyectos, listarPorConstructora, consultarProyecto, listarSimilares, listarAvances, crearProyecto, editarProyecto, registrarAvance, eliminarProyecto } from "../Controller/proyectosController.ts";
+import { authMiddleware, rolMiddleware } from "../Middlewares/validarJWT.ts";
 
 const proyectosRouter = new Router();
 
@@ -11,11 +12,11 @@ proyectosRouter.get("/proyectos/:id/similares", listarSimilares);
 proyectosRouter.get("/proyectos/:id/avances", listarAvances);
 
 //Rutas privadas (Constructora y administrador)
-proyectosRouter.post("/proyectos", crearProyecto );
-proyectosRouter.put("/proyectos/:id", editarProyecto);
-proyectosRouter.delete("/proyectos/:id", eliminarProyecto);
+proyectosRouter.post("/proyectos", authMiddleware,rolMiddleware ("Constructora", "Administrador"), crearProyecto );
+proyectosRouter.put("/proyectos/:id", authMiddleware,rolMiddleware ("Constructora", "Administrador"), editarProyecto);
+proyectosRouter.delete("/proyectos/:id", authMiddleware,rolMiddleware ("Constructora", "Administrador"), eliminarProyecto);
 
 // Ruta privada solo para constructora
-proyectosRouter.post("/proyectos/:id/avances", registrarAvance);
+proyectosRouter.post("/proyectos/:id/avances", authMiddleware,rolMiddleware ("Constructora"), registrarAvance);
 
 export default proyectosRouter;
