6ac221b9484c8ac2dd9599409f04863d1a69340e
andriugeova-dot <andriu.geova@gmail.com>
2026-09-24 17:22:03 -0500

    cambios andres

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import terminosRouter from "./Router/terminosRouter.ts";
 import propiedadesRouter from "./Router/propiedadesRouter.ts";
 import proyectosRouter from "./Router/proyectosRouter.ts";
 import { constructoraPanelRouter } from "./Router/constructoraPanelRouter.ts";
+import { send } from "./Dependencies/dependencias.ts";
 
 const app = new Application();
 
@@ -13,6 +14,21 @@ app.use(oakCors({
     origin: "http://localhost:4321",
     credentials: true,   // necesario para que las cookies de sesión pasen
 }));
+
+app.use(async (ctx, next) => {
+    if (ctx.request.url.pathname.startsWith("/uploads/")) {
+        try {
+        await send(ctx, ctx.request.url.pathname, {
+            root: Deno.cwd(),
+        });
+        } catch {
+        ctx.response.status = 404;
+        ctx.response.body = { error: "Archivo no encontrado" };
+        }
+        return;
+    }
+    await next();
+});
 
 // Registrar routers
 const routes = [serviciosRouter,constructoraRouter,authRouter,terminosRouter,propiedadesRouter, proyectosRouter,constructoraPanelRouter];
